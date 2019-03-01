68aff9a8c3bed42d3204e437a013dddfe26e76f9
Bas K <39069678+Basje12@users.noreply.github.com>
2019-03-01 23:34:36 +0100

    Card css functionality

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+TODO:
+- Create card
+- Create Set of chosen icons
+- Populate board
+
+- Click handler for cards (card--clicked)
+- Check if 2 cards match
+- Remove cards from board if match / Return to position if not card--completed
+*/
